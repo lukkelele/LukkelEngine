dc75d5a0134841aa65573eb69e8ef2a3dc62f3f0
lukkelele <encryptedluke@protonmail.com>
2022-07-16 10:19:07 +0200

    math added

--- GraphicsEngine/GraphicsEngine.cpp
+++ GraphicsEngine/GraphicsEngine.cpp
@@ -30,12 +30,34 @@ bool GraphicsEngine::onUserCreate() {
 		{ 0.0f, 0.0f, 0.0f,   1.0f, 0.0f, 1.0f,  1.0f, 0.0f, 0.0f }
 	};
 	// PROJECTION MATRIX
-	float f_near = 0.1f;
-	float f_far = 1000.f;
-	float f_fov = 90.f;
-	//float f_aspectRatio = 
+	float z_near = 0.1f;
+	float z_far = 1000.f;
+	float fov = 90.f;
+	float aspectRatio = getAspectRatio();
+	float fovRad = degreeToRadian(fov);
+	float fovRadian = 1.0f / tanf(fovRad * 0.5f); // might reuse fovRad here
+	// matProj.m[ROW][COLUMN]
+	matProj.m[0][0] = aspectRatio * fovRadian;
+	matProj.m[1][1] = fovRadian;
+	matProj.m[2][2] = z_far / (z_far - z_near);
+	matProj.m[2][3] = 1.0f;
+	matProj.m[3][2] = -(z_far * z_near) / (z_far - z_near);
+	matProj.m[3][3] = 0.0f;
 
 	return true;
+}
+
+// Matrix multiplication -> a * mat = b
+// mat[ROW][COLUMN]
+void GraphicsEngine::multiplyMatrix(vec3d& a, vec3d& b, mat4x4& mat) {
+	b.x = a.x * mat.m[0][0] + a.y * mat.m[1][0] + a.z * mat.m[2][0] + mat.m[3][0];
+	b.y = a.x * mat.m[0][1] + a.y * mat.m[1][1] + a.z * mat.m[2][1] + mat.m[3][1];
+	b.z = a.x * mat.m[0][2] + a.y * mat.m[1][2] + a.z * mat.m[2][2] + mat.m[3][2];
+}
+
+float GraphicsEngine::degreeToRadian(float degrees) {
+	// 180 degrees = 1 rad --> 1 degree = 1/180 rad
+	return degrees * (1 / 180.0f) * 3.14f;
 }
 
 bool GraphicsEngine::onUserUpdate(float elapsedTime) {
@@ -51,9 +73,14 @@ void GraphicsEngine::clearScreen() {
 }
 
 // Only works when on window is on primary monitor
-float GraphicsEngine::getScreenSize() {
+void GraphicsEngine::getScreenSize() {
 	GLFWmonitor* monitor = glfwGetPrimaryMonitor();
 	const GLFWvidmode* mode = glfwGetVideoMode(monitor);
 	this->height = mode->height;
 	this->width = mode->width;
+}
+
+float GraphicsEngine::getAspectRatio() {
+	getScreenSize();
+	return this->height / this->width;
 }
--- GraphicsEngine/GraphicsEngine.h
+++ GraphicsEngine/GraphicsEngine.h
@@ -2,8 +2,6 @@
 #define _GRAPHICS_ENGINE_H
 #include <vector>
 
-#define Z_FAR 
-#define Z_NEAR
 
 class GraphicsEngine {
 
@@ -11,24 +9,26 @@ public:
 	GraphicsEngine();
 
 public:
-	bool onUserCreate();
-	bool onUserUpdate(float elapsedTime);
-	void clearScreen();
-	float getScreenSize();
-	float getAspectRatio();
-	
-
-public:
-
 	struct vec3d { float x, y, z; };
 	struct triangle { vec3d p[3]; };
 	struct mesh { std::vector<triangle> tris; };
 	struct mat4x4 { float m[4][4] = { 0 }; };
 
 private:
-	int width;
 	int height;
+	int width;
 	mesh meshCube;
+	mat4x4 matProj;
+
+public:
+	bool onUserCreate();
+	bool onUserUpdate(float elapsedTime);
+	void clearScreen();
+	void getScreenSize();
+	float degreeToRadian(float degrees);
+	float getAspectRatio();
+	void multiplyMatrix(vec3d &a, vec3d &b, mat4x4 &mat);
+
 
 };
 

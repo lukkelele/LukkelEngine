1697f6c41cf7b6ddf65d8e946e776595bf67051e
lukkelele <encryptedluke@protonmail.com>
2023-02-01 18:15:35 +0100

    Fixed VertexBufferLayout problems

--- LukkelEngine/include/Renderer/VertexArray.h
+++ LukkelEngine/include/Renderer/VertexArray.h
@@ -1,17 +1,8 @@
 #pragma once
 
 #include <GLErrorHandler.h>
-
 #include <VertexBuffer.h>
-#if defined _WINDOWS_x64 || _WINDOWS_x86
-  #include <VertexBufferLayout_WINDOWS.h>
-  typedef VertexBufferLayout_WINDOWS VertexBufferLayout;
-#else
-  #include <VertexBufferLayout_LINUX.h>
-  typedef VertexBufferLayout_LINUX VertexBufferLayout;
-#endif
-// #include <VertexBufferLayout.h>
-
+#include <VertexBufferLayout.h>
 
 class VertexArray
 {

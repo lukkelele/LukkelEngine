01c558684c4f80fb0df60d9f94b2ccdf3ca229c9
lukkelele <encryptedluke@protonmail.com>
2023-02-08 16:07:14 +0100

    New camera implementation

--- LukkelEngine/src/LukkelEngine/Renderer/Camera.h
+++ LukkelEngine/src/LukkelEngine/Renderer/Camera.h
@@ -14,29 +14,24 @@ namespace LukkelEngine {
 	class Camera
 	{
 	public:
-		// Stores the main vectors of the camera
-		glm::vec3 Position;
-		glm::vec3 Orientation = glm::vec3(0.0f, 0.0f, -1.0f);
-		glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f);
+		Camera(float FOV, float width, float height, float nearPlane,
+			   float farPlane, glm::vec3 camPos);
+		~Camera();
 
-		// Prevents the camera from jumping around when first clicking left click
-		bool firstClick = true;
+		glm::vec3 getCameraPosition();
+		glm::mat4 getProjectionMatrix();
+		glm::mat4 getViewMatrix();
 
-		// Stores the width and height of the window
-		int m_Width;
-		int m_Height;
+	private:
+		glm::vec3 m_Pos;
+		float m_FOV;
+		float m_Width, m_Height;
+		float m_NearPlane, m_FarPlane;
+		glm::vec3 m_Front = glm::vec3(0.0f, 0.0f, 0.0f);
+		glm::vec3 m_Up = glm::vec3(0.0f, 1.0f, 0.0f);
 
-		// Adjust the speed of the camera and it's sensitivity when looking around
-		float m_Speed = 0.1f;
-		float m_Sensitivity = 100.0f;
-
-		// Camera constructor to set up initial values
-		Camera(int width, int height, glm::vec3 position);
-
-		// Updates and exports the camera matrix to the Vertex Shader
-		void Matrix(float FOVdeg, float nearPlane, float farPlane, Shader& shader, const char* uniform);
-		// Handles camera inputs
-		void Inputs(GLFWwindow* window);
+		glm::mat4 m_ViewMat;
+		glm::mat4 m_ProjMat;
 	};
 
 }

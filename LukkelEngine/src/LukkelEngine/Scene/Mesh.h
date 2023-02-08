29c76ed98628ced7a18525a8d022693b563c0529
lukkelele <encryptedluke@protonmail.com>
2023-02-08 16:07:36 +0100

    Basic mesh operations in a new file named Mesh.h

---
+++ LukkelEngine/src/LukkelEngine/Scene/Mesh.h
@@ -0,0 +1,31 @@
+#include <vector>
+#include <glm/glm.hpp>
+
+namespace LukkelEngine {
+
+	enum MeshType
+	{
+		Triangle = 0,
+		Quad = 1,
+		Cube = 2,
+		Sphere = 3
+	};
+
+	struct Vertex
+	{
+		glm::vec3 m_Pos;
+		glm::vec3 m_Normal;
+		glm::vec3 m_Color;
+		glm::vec2 m_texCoords;
+	};
+
+	class Mesh
+	{
+	public:
+		static void setTriangleData(std::vector<Vertex>& vertices, std::vector<uint32_t> &indices);
+		static void setQuadData(std::vector<Vertex>& vertices, std::vector<uint32_t> &indices);
+		static void setCubeData(std::vector<Vertex>& vertices, std::vector<uint32_t> &indices);
+		static void setSphereData(std::vector<Vertex>& vertices, std::vector<uint32_t> &indices);
+	};
+
+}
